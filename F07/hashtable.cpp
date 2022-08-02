b1b9dd957d6a1574c70adb7756858ea29bf027e6
Wasif Jalal <jalalwasif@gmail.com>
2022-08-02 14:51:40 +0600

    f07 start

---
+++ F07/hashtable.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+long long int power (long long int a, long long int b) {
+    if (b == 0) return 1;
+    long long int x = power (a, b/2);
+    x *= x;;
+    if (b%2 == 1) x *= a;
+    return x;
+};
+
+// unsigned int h2 (char* a, int N) {
+//     int i;
+//     unsigned long long int x = 1;
+//     for (i=0; a[i]!='\0'; i++) x *= (a[i] - 'a' + 1);
+//     return (unsigned int) (x % N);
+// };
+
+unsigned int h2 (char* a, int N) {
+    unsigned long long int x = 5381;
+    while (int c = *a++) x = ((x << 5) + x) + c;
+    return (unsigned int) (x % N);
+}
+
+unsigned int h1 (char* a, int N) {
+    int i, z = 31;
+    long long int x = 0;
+    for (i=0; a[i]!='\0'; i++) x += a[i] * power (z, i);
+    return (unsigned int) (x % N);
+};
+
+double successRate (unsigned int (*h)(char*, int)) {
+    double avg = 0; int p;
+    for (p=0; p<20000; p++) {
+        srand (time(0));
+        int i, j, done[100], unique = 0;
+        char s[8] = "       ";
+        for (i=0; i<100; i++) done[i] = -1;
+        for (i=0; i<100; i++) {
+            for (j=0; j<7; j++) s[j] = 97 + abs(rand()) % 25;
+            //cout << s << " " << h(s, 100) << endl;
+            done[h(s, 100)]++;
+        };
+        for (i=0; i<100; i++) 
+            if (done[i] >= 0) unique++;
+        avg += 1.0 * (unique);
+    };
+    return avg/20000;
+}
+
+int main() {
+    cout << "h1: " << successRate (&h1) << endl;
+    cout << "h2: " << successRate (&h2) << endl;
+}
+
